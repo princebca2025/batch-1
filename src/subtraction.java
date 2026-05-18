e9d23e385d979e186fe2177ac81666224d6966ec
princebca2025 <princeabhirana@gmail.com>
2026-05-18 14:02:38 +0530

    adding substraction file

---
+++ src/subtraction.java
@@ -0,0 +1,7 @@
+public class subtraction {
+    public static void main(String[] args) {
+        int a=87,b=67,c;
+        c=a-b;
+        System.out.println("print the value of c is"+c);
+    }
+}
