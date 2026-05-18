16f9722601beb28b4685fd6e4767fce326c02274
princebca2025 <princeabhirana@gmail.com>
2026-05-18 14:24:54 +0530

    adding multiplication file

---
+++ src/multiplication.java
@@ -0,0 +1,7 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int a=7,b=5,c;
+        c=a*b;
+        System.out.println("print the value of c is"+c);
+    }
+}
