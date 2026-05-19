19913c0efba827e0a680060ec24afc0428262c25
princebca2025 <princeabhirana@gmail.com>
2026-05-19 17:20:43 +0530

    adding students details file

--- src/StudentDetails.java
+++ src/StudentDetails.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class StudentDetails {
+    public static void main(String[] args) {
+        int maths=83,english=76,hindi=85,biology=87,physics=79,total=0;
+        total= maths+english+hindi+biology+physics;
+        System.out.println(" maths="+maths+"\n english="+english+"\n hindi="+hindi+"\n biology="+biology+"\n physics="+physics+"\n tota "+total );
+
+
+    }
 }
