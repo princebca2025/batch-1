bb3cdf56531bafe617eb4b67adbf60469cd46758
princebca2025 <princeabhirana@gmail.com>
2026-05-20 13:19:27 +0530

    adding swapping filw

--- src/Swapping.java
+++ src/Swapping.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Swapping {
+    public static void main(String[] args) {
+        int a=120,b=70,c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("the value of a is="+a);
+        System.out.println("the value of b is="+b);
+        System.out.println("the value of c is="+c);
+    }
 }
